da26d891c042cc5197a664abe9cdc16f8eaade6d
deekshi-1 <deekshithc1@gmail.com>
2025-10-15 13:50:15 +0530

    g

---
+++ backend/express/basic2/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const fs = require("fs");
+
+app.use(bodyparser.json());
+
+app.get('')
+
+app.listen(3000);
